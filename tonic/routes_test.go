1521f8295318518e1a52a42acf21b281a06070ca
Achilleas Koutsou <achilleas.k@gmail.com>
2020-11-24 16:38:47 +0100

    [Test] Job log rendering and job count

--- tonic/routes_test.go
+++ tonic/routes_test.go
@@ -1,7 +1,9 @@
 package tonic
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -95,4 +97,54 @@ func TestFormRoutes(t *testing.T) {
 	if status := rr.Code; status != http.StatusSeeOther {
 		t.Errorf("handler returned wrong status code: got %v expected %v", status, http.StatusSeeOther)
 	}
+}
+
+func TestLogRoutes(t *testing.T) {
+	f := new(form.Form)
+	f.Pages = []form.Page{{Elements: make([]form.Element, 1)}}
+	srv, err := NewService(*f, nil, echoAction, Config{CookieName: "test-cookie"})
+	if err != nil {
+		t.Fatalf("failed to initialise tonic service: %s", err.Error())
+	}
+	handler := srv.web.Handler
+
+	// Add test cookie to the database
+	testSession := db.NewSession("test-token", 42)
+	srv.db.InsertSession(testSession)
+
+	cookie := testSession.ID
+
+	jobLabel := "TestJob"
+
+	checkLogJobCount := func(route string, nexpected int) {
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", route, nil)
+		if err != nil {
+			t.Errorf("failed to create request: %s", route)
+		}
+		req.Header.Add("Cookie", fmt.Sprintf("test-cookie=%s", cookie))
+		handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v expected %v", status, http.StatusOK)
+		}
+
+		content, err := ioutil.ReadAll(rr.Body)
+		if err != nil {
+			t.Errorf("failed to read response body: %s", err.Error())
+		}
+		if njobs := bytes.Count(content, []byte(jobLabel)); njobs != nexpected {
+			t.Errorf("Job log returned %d, expected %d", njobs, nexpected)
+		}
+	}
+
+	checkLogJobCount("/log", 0)
+
+	srv.db.InsertJob(&db.Job{ID: 12, UserID: 42, Label: jobLabel})
+	checkLogJobCount("/log", 1)
+
+	srv.db.InsertJob(&db.Job{ID: 16, UserID: 42, Label: jobLabel})
+	checkLogJobCount("/log", 2)
+
+	srv.db.InsertJob(&db.Job{ID: 26, UserID: 44, Label: jobLabel}) // other user
+	checkLogJobCount("/log", 2)
 }
